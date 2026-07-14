98ec4488be8473e7a1202901c7d9f72e0881f99c
Lance Nines <lance.nines@gmail.com>
2026-07-13 23:31:24 -0400

    init

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/ninesl/portfolio-site
+
+go 1.26.4
+
+tool (
+	github.com/a-h/templ/cmd/templ
+	github.com/air-verse/air
+)
+
+require (
+	dario.cat/mergo v1.0.2 // indirect
+	github.com/a-h/parse v0.0.0-20250122154542-74294addb73e // indirect
+	github.com/a-h/templ v0.3.1020 // indirect
+	github.com/air-verse/air v1.65.3 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/bep/godartsass/v2 v2.5.0 // indirect
+	github.com/bep/golibsass v1.2.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cli/browser v1.3.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gohugoio/hugo v0.149.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/natefinch/atomic v1.0.1 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/spf13/cast v1.9.2 // indirect
+	github.com/tdewolff/parse/v2 v2.8.3 // indirect
+	golang.org/x/mod v0.32.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/tools v0.41.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/ninesl/portfolio-site/pages"
+)
+
+var (
+	//go:embed assets
+	embeddedFiles embed.FS
+
+	assetHandler http.Handler
+
+	pageConfig = &pages.LayoutConfig{
+		Title: "Lance Nines - ninescoding",
+	}
+)
+
+func init() {
+	assets, err := fs.Sub(embeddedFiles, "assets")
+	if err != nil {
+		log.Fatal(err)
+	}
+	assetHandler = http.StripPrefix("/assets/", http.FileServerFS(assets))
+}
+
+func logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func addMiddleware(next http.Handler) http.Handler {
+	return logging(next)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /assets/{path...}", serveAsset)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		renderComponent(pages.Layout(pages.HomePage(), *pageConfig), w, r)
+	})
+	mux.HandleFunc("POST /count", func(w http.ResponseWriter, r *http.Request) {
+		pageConfig.COUNT++
+		renderComponent(pages.PersistCounter(pageConfig.COUNT, true), w, r)
+	})
+
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", addMiddleware(mux)))
+}
+
+func renderComponent(comp templ.Component, w http.ResponseWriter, r *http.Request) {
+	err := comp.Render(r.Context(), w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func serveAsset(w http.ResponseWriter, r *http.Request) {
+	assetHandler.ServeHTTP(w, r)
+}
